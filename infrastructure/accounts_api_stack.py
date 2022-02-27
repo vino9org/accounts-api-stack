3623399cb07661eea288482e9de85e782427bcb4
Li Lin <guru.lin@gmail.com>
2022-02-27 15:21:37 +0800

    Feature/1001 implement ddb logic (#1)
    * added appsync api with resolver. 
    * added integration test for apis

--- infrastructure/accounts_api_stack.py
+++ infrastructure/accounts_api_stack.py
@@ -1,12 +1,12 @@
-from os.path import abspath, dirname
+import os.path
+from typing import Tuple
 
-from aws_cdk import Stack
-from aws_cdk import aws_apigateway as apigateway
-from aws_cdk import aws_lambda as _lambda
-from aws_cdk import aws_logs as logs
-
-
+import aws_cdk.aws_appsync_alpha as appsync
+from aws_cdk import CfnOutput, RemovalPolicy, Stack
+from aws_cdk import aws_dynamodb as dynamodb
 from constructs import Construct
+
+resolver_path = os.path.dirname(__file__) + "/resolvers"
 
 
 class AccountsApiStack(Stack):
@@ -14,7 +14,46 @@ class AccountsApiStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
 
     def build(self):
-        
+        api, table = self.build_appsync_accounts()
+        CfnOutput(self, "AccountsApiUrl", value=api.graphql_url)
+        CfnOutput(self, "AccountsTableName", value=table.table_name)
         return self
 
-    
+    def build_appsync_accounts(self) -> Tuple[appsync.GraphqlApi, dynamodb.Table]:
+        schema_dir = os.path.abspath(os.path.dirname(__file__))
+        api = appsync.GraphqlApi(
+            self,
+            "Api",
+            name="accounts",
+            schema=appsync.Schema.from_asset(f"{schema_dir}/schema.graphql"),
+            authorization_config=appsync.AuthorizationConfig(
+                default_authorization=appsync.AuthorizationMode(authorization_type=appsync.AuthorizationType.IAM)
+            ),
+            xray_enabled=True,
+            log_config=appsync.LogConfig(
+                field_log_level=appsync.FieldLogLevel.ALL,
+            ),
+        )
+        api.apply_removal_policy(RemovalPolicy.DESTROY)
+
+        account_table = dynamodb.Table(
+            self,
+            "AccountTable",
+            partition_key=dynamodb.Attribute(name="customer_id", type=dynamodb.AttributeType.STRING),
+            sort_key=dynamodb.Attribute(name="id", type=dynamodb.AttributeType.STRING),
+            billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST,
+            removal_policy=RemovalPolicy.DESTROY,
+        )
+
+        account_dS = api.add_dynamo_db_data_source("accountDS", account_table)
+
+        account_dS.create_resolver(
+            type_name="Query",
+            field_name="getAccountsForCustomer",
+            request_mapping_template=appsync.MappingTemplate.from_file(
+                f"{resolver_path}/get_accounts_for_customer.vtl"
+            ),
+            response_mapping_template=appsync.MappingTemplate.dynamo_db_result_list(),
+        )
+
+        return api, account_table
---
+++ tests/integration/conftest.py
@@ -0,0 +1,116 @@
+import os
+from decimal import Decimal
+from typing import List
+
+import boto3
+import pytest
+import test_constants as constants
+from botocore.exceptions import ClientError
+from requests_aws4auth import AWS4Auth
+
+_stack_outputs_: List[str] = {}
+
+
+@pytest.fixture(scope="session")
+def api_auth() -> str:
+    return iam_auth_for_service("appsync")
+
+
+@pytest.fixture(scope="session")
+def api_url() -> str:
+    return stack_outputs_for_key("AccountsApiUrl")[0]
+
+
+@pytest.fixture(scope="session")
+def accounts_table_name() -> str:
+    return stack_outputs_for_key("AccountsTableName")[0]
+
+
+@pytest.fixture(autouse=True)
+def seed_data(accounts_table_name):
+    """ensure seed data is in database"""
+    print("......seeding test data.....")
+    ddb = boto3.resource("dynamodb")
+    table = ddb.Table(accounts_table_name)
+    table.put_item(
+        Item={
+            "customer_id": constants.TEST_CUSTOMER_ID_1,
+            "id": constants.TEST_ACCOUNT_ID_1,
+            "name": "Magic Saving Account",
+            "prod_code": "SAV001",
+            "ledger_balance": Decimal(12345678.90),
+            "avail_balance": Decimal(12345600.01),
+            "currency": "SGD",
+            "status": "active",
+            "last_updated": "2022-02-27T13:20:03.126945Z",
+        }
+    )
+
+
+def stack_outputs_for_key(key: str) -> List[str]:
+    """
+    helper funciton to get output values from a Cloudformation stack
+    can be used by a fixture to retrieve output values and inject
+    into tests
+
+    e.g.
+
+    # in conftest.py
+    @pytest.fixture(scope="session")
+    def api_base_url() -> str:
+        return _stack_outputs__for_key("RestApiEndpoint")[0]
+
+    # in tests
+    import requests
+    def test_restapi(api_base_url):
+        response = requests.get(f"{api_base_url}/ping")
+        assert response.status_code == 200
+
+    """
+
+    global _stack_outputs_
+
+    region = os.environ.get("TESTING_REGION", "us-west-2")
+    stack_name = os.environ.get("TESTING_STACK_NAME", "AccountsApiStack")
+    client = boto3.client("cloudformation", region_name=region)
+
+    if not _stack_outputs_:
+        try:
+            response = client.describe_stacks(StackName=stack_name)
+            _stack_outputs_ = response["Stacks"][0]["Outputs"]
+        except ClientError as e:
+            raise Exception(f"Cannot find stack {stack_name} in region {region}") from e
+
+    output_values = [item["OutputValue"] for item in _stack_outputs_ if key in item["OutputKey"]]
+    if not output_values:
+        raise Exception(f"There is no output with key {key} in stack {stack_name} in region {region}")
+
+    return output_values
+
+
+def iam_auth_for_service(service: str) -> AWS4Auth:
+    """
+    create the auth object for signing a HTTP request with AWS IAM v4 signature
+    can be used create fixture for test cases where IAM authentication is used
+
+    e.g.
+
+    # in conftest.py
+    @pytest.fixture(scope="session")
+    def http_api_authj() -> str:
+        return iam_auth_for_service("execute-api")
+
+    # in tests
+    def test_restapi(api_base_url, http_api_auth):
+        response = requests.get(f"{api_base_url}/ping", auth=http_api_auth)
+        assert response.status_code == 200
+
+    """
+    session = boto3.Session()
+    credentials = session.get_credentials()
+    return AWS4Auth(
+        credentials.access_key,
+        credentials.secret_key,
+        session.region_name,
+        service,
+    )
