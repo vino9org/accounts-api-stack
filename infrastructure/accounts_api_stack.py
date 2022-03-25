c3ccd1f32e15b3564545322073c1b372c98fad05
Li Lin <guru.lin@gmail.com>
2022-03-26 00:24:00 +0800

    Feature/1004 use pydantic model (#5)
    use pydantic model to event

--- infrastructure/accounts_api_stack.py
+++ infrastructure/accounts_api_stack.py
@@ -6,6 +6,7 @@ from aws_cdk import aws_dynamodb as dynamodb
 from aws_cdk import aws_events as events
 from aws_cdk import aws_lambda as _lambda
 from aws_cdk import aws_logs as logs
+from aws_cdk import aws_sam as sam
 from aws_solutions_constructs.aws_eventbridge_lambda import EventbridgeToLambda
 from aws_solutions_constructs.aws_lambda_dynamodb import LambdaToDynamoDB
 from constructs import Construct
@@ -84,13 +85,7 @@ class AccountsApiStack(Stack):
                 runtime=_lambda.Runtime.PYTHON_3_9,
                 handler="app.lambda_handler",
                 code=_lambda.Code.from_asset(src_dir),
-                layers=[
-                    _lambda.LayerVersion.from_layer_version_arn(
-                        self,
-                        "lambda-powertools-layer",
-                        f"arn:aws:lambda:{Stack.of(self).region}:017000801446:layer:AWSLambdaPowertoolsPython:10",
-                    )
-                ],
+                layers=[self.powertools_layer("1.24.2")],
                 memory_size=512,
                 architecture=_lambda.Architecture.ARM_64,
                 log_retention=logs.RetentionDays.ONE_WEEK,
@@ -110,3 +105,17 @@ class AccountsApiStack(Stack):
                 rule_name=f"{self.stack_name}-update-trigger",
             ),
         )
+
+    def powertools_layer(self, version: str) -> _lambda.ILayerVersion:
+        # Launches SAR App as CloudFormation nested stack and return Lambda Layer
+        POWERTOOLS_BASE_NAME = "AWSLambdaPowertools"
+        powertools_app = sam.CfnApplication(
+            self,
+            f"{POWERTOOLS_BASE_NAME}Application",
+            location={  # type: ignore
+                "applicationId": "arn:aws:serverlessrepo:eu-west-1:057560766410:applications/aws-lambda-powertools-python-layer-extras",  # noqa
+                "semanticVersion": version,
+            },
+        )
+        powertools_layer_arn = powertools_app.get_att("Outputs.LayerVersionArn").to_string()
+        return _lambda.LayerVersion.from_layer_version_arn(self, f"{POWERTOOLS_BASE_NAME}", powertools_layer_arn)
--- tests/unit/runtime/test_event_handler.py
+++ tests/unit/runtime/test_event_handler.py
@@ -1,6 +1,7 @@
 import json
 import uuid
 from datetime import datetime
+from decimal import Decimal
 from typing import Any, Dict, Tuple
 
 import app
@@ -9,21 +10,26 @@ import utils
 
 def gen_test_event() -> Tuple[str, Dict[Any, Any]]:
     trx_id = uuid.uuid1().hex
-    detail = json.dumps(
-        {
-            "customer_id": utils.TEST_CUSTOMER_ID_1,
-            "account_id": utils.TEST_ACCOUNT_ID_1,
-            "transaction_id": trx_id,
-            "transfer_amount": 1234.56,
-            "previous_balance": 10000.00,
-            "previous_avail_balance": 10000.00,
-            "new_balance": 11234.56,
-            "new_avail_balance": 11234.56,
-            "currency": "SGD",
-            "memo": "to some random account",
-            "transaction_date": "2022-02-27",
-            "status": "completed",
-        }
+    transfer = app.FundTransfer(
+        transaction_id=trx_id,
+        debit_customer_id=utils.TEST_CUSTOMER_ID_1,
+        debit_account_id=utils.TEST_ACCOUNT_ID_1,
+        currency="SGD",
+        credit_customer_id="omitted",
+        credit_account_id="omitted",
+        transfer_amount=Decimal("99.98"),
+        memo="test transfer from pytest",
+        transaction_date="2022-03-21",
+        debit_prev_balance=Decimal("1000.12"),
+        debit_prev_avail_balance=Decimal("1000.12"),
+        debit_balance=Decimal("900.14"),
+        debit_avail_balance=Decimal("900.14"),
+        credit_prev_balance=Decimal("2000.00"),
+        credit_prev_avail_balance=Decimal("2000.00"),
+        credit_balance=Decimal("2099.98"),
+        credit_avail_balance=Decimal("2099.98"),
+        status="completed",
+        limits_req_id="AAAA",
     )
 
     event = {
@@ -31,7 +37,7 @@ def gen_test_event() -> Tuple[str, Dict[Any, Any]]:
         "Source": "service.fund_transfer",
         "DetailType": "transfer",
         "EventBusName": "default",
-        "Detail": detail,
+        "Detail": transfer.json(),
     }
 
     return trx_id, event
@@ -39,5 +45,6 @@ def gen_test_event() -> Tuple[str, Dict[Any, Any]]:
 
 def test_handler_for_eventbridge(ddb_table):
     _, event = gen_test_event()
-    result = app.process_transfer_event(json.loads(event["Detail"]), ddb_table)
+    transfer = app.FundTransfer(**json.loads(event["Detail"]))
+    result = app.process_transfer_event(transfer, ddb_table)
     assert result
